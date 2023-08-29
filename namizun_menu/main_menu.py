fb1d32c2dd76a55236fb858c33cbc3eedfab547f
Ali-Flt <mr.ali1377@yahoo.com>
2023-08-30 02:31:12 +0330

    fixed a bug in cron

--- namizun_menu/main_menu.py
+++ namizun_menu/main_menu.py
@@ -20,7 +20,7 @@ def cron_setter():
     selection = int(input("\nNumber of minutes per hour?"))
     
     if 0 < selection <= 30:
-        cron = CronTab(user=True)
+        cron = cron_remover()
         start_mins = np.linspace(0, 58, selection, dtype=int)
         stop_mins = start_mins + 1
         for start, end in zip(start_mins, stop_mins):
@@ -35,14 +35,13 @@ def cron_setter():
     else:
         return cron_setter()
 
-
 def cron_remover():
     cron = CronTab(user=True)
     iter = cron.find_command('namizun')
     for job in iter:
         cron.remove(job)
     cron.write()
-
+    return cron
 
 def menu():
     display.banner()
